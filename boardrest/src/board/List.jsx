1c8637f2d6788e14ceaee20d3bd9e9b6ead97d5c
kys <tiger2ys98@gmail.com>
2025-08-28 15:06:45 +0900

    검색처리

--- boardrest/src/board/List.jsx
+++ boardrest/src/board/List.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';//서버에서 데이터만 가져올때 사용하는 객체
-import {Link, useNavigate} from 'react-router-dom';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
 import Button from "bootstrap/js/src/button";
 
 
@@ -15,11 +15,15 @@ function List(props) {
   const [size,setSize] = useState(10);
   const [start,setStart] = useState(1);
   const [totalPage,setTotalPage]  = useState(1);
+  const [searchOption, setSearchOption] = useState('');
+  const [searchString, setSearchString] = useState('');
+  const location = useLocation();
+  const [searchCount, setSearchCount] = useState(0);
 
   //List컴포넌트가 생성될때 한번만 호출하도록 구성
   useEffect(() => {
     //page=2&size=10&type=&keyword=
-    axios.get(`boardrest/list?page=${page}&size=${size}&type="&keyword=`)
+    axios.get(`boardrest/list?page=${page}&size=${size}&type=${searchOption}&keyword=${searchString}`)
     .then((res)=>{
       console.log(res);
       console.log(list);
@@ -38,7 +42,7 @@ function List(props) {
     .catch((err)=>{
       console.log("error:",err);
     });
-  }, [page]);
+  }, [page, searchCount]);
 
   const handleClick = (e) => {
     const {name} = e.target;
@@ -52,8 +56,39 @@ function List(props) {
       setPage(name);
     }
   }
+  const handleChangeSelect = (event) => {
+    setSearchOption(event.target.value);
+  }
+
+  const handleChangeString = (event) => {
+    setSearchString(event.target.value);
+  }
+
+  const handleClickSearch = (event) => {
+    // setPage(1);
+    navigate({
+      pathname: location.pathname,
+      search: `?page=${page}&size=${size}&type=${searchOption}&keyword=${searchString}`,
+    });
+    setSearchCount(searchCount+1);
+  }
+
+
 
   return (
+    <div>
+      <div>
+        <select value={searchOption} onChange={handleChangeSelect} >
+          <option value=''>----------</option>
+          <option value='t'>제목</option>
+          <option value='c'>내용</option>
+          <option value='w'>작성자</option>
+          <option value='tc'>제목+내용</option>
+          <option value='tcw'>제목+내용+작성자</option>
+        </select>
+        <input type='text' value={searchString} onChange={handleChangeString}  />
+        <button type="button" onClick={handleClickSearch} >검색</button>
+      </div>
       <table className="table table-hover">
         <thead>
           <tr>
@@ -91,6 +126,7 @@ function List(props) {
           </tr>
         </tfoot>
       </table>
+    </div>
   );
 
 }
